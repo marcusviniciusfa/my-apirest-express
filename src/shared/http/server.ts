286bbbb959cdffb95cf234cdfb046aee4943ee37
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-11-19 16:26:36 -0300

    feat: add swagger initial config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,27 +1,31 @@
 import { AppError } from '@shared/errors/AppError'
 import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../../swagger.json'
 import { routes } from './routes'
 
 const PORT = process.env.PORT
 
 const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
-  console.error(error)
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
+app
+  .use(cors())
+  .use(express.json())
+  .use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+  .use(routes)
+  .use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(error)
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      })
+    }
+    return res.status(500).json({
       status: 'error',
-      message: error.message,
+      message: 'Internal server error',
     })
-  }
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
   })
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening 👂 on port ${PORT}`)
