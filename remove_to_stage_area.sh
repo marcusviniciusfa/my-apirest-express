84d04eb75d54b831d46d5f9ce15a4f4f3a216ed5
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2023-01-25 20:06:49 -0300

    chore: add possibility to pass more files to be unstaged

--- remove_to_stage_area.sh
+++ remove_to_stage_area.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git restore --staged $1 > /dev/null 2>&1
+git restore --staged $* > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-  echo "removed $1 from the stage area"
+  echo "successfully removed $* from the stage area 🎉"
 fi
 exit 0
