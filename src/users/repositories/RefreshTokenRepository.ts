51b275dbc1d7892288f6742b629c6dd92879e75e
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2023-01-30 15:46:56 -0300

    feat: add RefreshTokenRepository implementation

---
+++ src/users/repositories/RefreshTokenRepository.ts
@@ -0,0 +1,25 @@
+import { dataSource } from '@/shared/typeorm'
+import { Repository } from 'typeorm'
+import { RefreshToken } from '../database/entities/RefreshToken'
+import { CreateRefreshTokenDTO } from '../dtos/CreateRefreshTokenDTO'
+
+export class IRefreshTokenRepository implements IRefreshTokenRepository {
+  private readonly repository: Repository<RefreshToken>
+
+  constructor() {
+    this.repository = dataSource.getRepository(RefreshToken)
+  }
+
+  async create({ token, valid, expires, userId }: CreateRefreshTokenDTO): Promise<RefreshToken> {
+    const refreshToken = this.repository.create(new RefreshToken(token, valid, expires, userId))
+    return this.repository.save(refreshToken)
+  }
+
+  async findByToken(token: string): Promise<RefreshToken | null> {
+    return this.repository.findOneBy({ token })
+  }
+
+  async update(refreshToken: RefreshToken): Promise<void> {
+    await this.repository.save(refreshToken)
+  }
+}
